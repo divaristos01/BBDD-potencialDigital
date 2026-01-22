04191b92e038c5aabe93c00decdfadf18e09acd9
divaristos01 <diegoevaristosantiago.guadalupe@alumnado.fundacionloyola.net>
2026-01-22 20:40:48 +0100

    Delete ERROR CHECK.sql

--- ERROR CHECK.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO charla(nombre, descripcion, media, idPonente)
-VALUES ('SQL AVANZADO', 'Conceptos avanzados de SQL Server', 11, 1); --ERROR YA QUE NO ENTRA DENTRO DEL INTERVALO DEL CHECK
