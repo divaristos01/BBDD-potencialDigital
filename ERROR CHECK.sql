0221b92a87dd2d0a389add938672cf1d26bfb67f
divaristos01 <diegoevaristosantiago.guadalupe@alumnado.fundacionloyola.net>
2026-01-22 20:38:01 +0100

    Add files via upload

---
+++ ERROR CHECK.sql
@@ -0,0 +1,2 @@
+INSERT INTO charla(nombre, descripcion, media, idPonente)
+VALUES ('SQL AVANZADO', 'Conceptos avanzados de SQL Server', 11, 1); --ERROR YA QUE NO ENTRA DENTRO DEL INTERVALO DEL CHECK
